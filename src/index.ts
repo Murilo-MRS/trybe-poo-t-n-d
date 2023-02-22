dd30252624496f00ff1c87ff6009714225259059
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-22 20:00:57 -0300

    feat: req-13 teste instancias

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Lordao Vasco');
+const player2 = new Character('Dedo de Mel');
+const player3 = new Character('Piolheira');
+
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => battle.fight());
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
